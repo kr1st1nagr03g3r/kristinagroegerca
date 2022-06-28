577e5284b6ebed3d7de7aa9c66a0b8b496b3cec2
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-06-28 17:46:28 -0400

    nav styles

---
+++ components/Navigation.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import styles from '../styles/Nav.module.css'
+
+
+class Navigation extends React.Component {
+    state = {  } 
+    render() { 
+        return (
+
+<>
+<div className={styles.height}>
+
+</div>
+
+
+</>
+
+        );
+    }
+}
+ 
+export default Navigation;
