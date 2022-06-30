7208cf620fbcb7c3797fa11d1885f11383e20517
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-06-30 18:02:46 -0400

    animation module css

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 // import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import styles from '../styles/Animations.module.css'
 import Navigation from '../components/Navigation'
 import Container from '../components/Container'
 import Hero from '../components/Hero'
+import { useInView } from 'react-intersection-observer';
+
+
 
 export default function Home() {
+  const { ref: magicSectionRef, inView: magicSectionIsVisible } = useInView();
   return (
 
     <div>
@@ -19,19 +23,24 @@ export default function Home() {
         <h1>Web Developer<br></br>
           &amp; Designer.</h1>
         <p>Idk wtf to say here
-        Kristina is a UI and UX developer &amp; designer who has pushed pixels and made web products since 2012.
+        Kristina is a UI and UX developer &amp; designer who has pushed pixels and made web products since 2012. 
+        I&apos;ve spent a large amount of that time working on various CMS&apos;s, primarily Adobe Experience Manager.
       </p>
       </Hero>
       <Container>
-        <h3>Id aliqua magna esse sunt. Aliqua officia enim veniam pariatur.</h3>
+        <h3 >Id aliqua magna esse sunt. Aliqua officia enim veniam pariatur.</h3>
         <p>Incididunt sunt pariatur in velit Lorem eiusmod non. Consectetur incididunt deserunt enim adipisicing ut cupidatat elit est labore officia sint in ipsum enim. Mollit enim quis ea mollit dolor proident est veniam tempor nisi deserunt. Voluptate id voluptate amet cupidatat excepteur minim ipsum aliqua incididunt et laborum culpa ut.</p>
+
       </Container>
       <Container>
         <h3>Id aliqua magna esse sunt. Aliqua officia enim veniam pariatur.</h3>
         <p>Incididunt sunt pariatur in velit Lorem eiusmod non. Consectetur incididunt deserunt enim adipisicing ut cupidatat elit est labore officia sint in ipsum enim. Mollit enim quis ea mollit dolor proident est veniam tempor nisi deserunt. Voluptate id voluptate amet cupidatat excepteur minim ipsum aliqua incididunt et laborum culpa ut.</p>
       </Container>
       <Container>
-        <h3>Id aliqua magna esse sunt. Aliqua officia enim veniam pariatur.</h3>
+      <span className={`${styles.rocket} ${magicSectionIsVisible ? styles.animateRocket : ''}`}>
+                            🚀
+                        </span>
+        <h3 ref={magicSectionRef}>Id aliqua magna esse sunt. Aliqua officia enim veniam pariatur.</h3>
         <p>Incididunt sunt pariatur in velit Lorem eiusmod non. Consectetur incididunt deserunt enim adipisicing ut cupidatat elit est labore officia sint in ipsum enim. Mollit enim quis ea mollit dolor proident est veniam tempor nisi deserunt. Voluptate id voluptate amet cupidatat excepteur minim ipsum aliqua incididunt et laborum culpa ut.</p>
       </Container>
 
