e204117b452292e21fbd4af40b2ac0c82bf8c80a
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-07-05 11:38:03 -0400

    Eyeballs

--- components/Eyeball.js
+++ components/Eyeball.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import useMightyMouse from 'react-hook-mighty-mouse';
-import styles from '../styles/Nav.module.css';
+import React, { useState } from "react";
+// import ReactDOM from "react-dom";
+import useMightyMouse from "react-hook-mighty-mouse";
+import styles from '../styles/Eyeball.module.css'
+import { Svg, Circle, Rect, Ellipse } from 'react-svg-path';
 
 
 const EyesFollow = () => {
@@ -16,19 +18,33 @@ const EyesFollow = () => {
   // } = useMightyMouse(true, 'right-eye', { x: 20, y: 20 });
 
   const rotateLeftEye = `rotate(${-angleLeftEye}deg)`;
+  // Remove the second eye for now
   // const rotateRightEye = `rotate(${-angleRightEye}deg)`;
 
   return (
-    <div className={styles.eyesFollow}>
-      <div className={styles.eyes}>
-        <div id="left-eye" className={styles.eye} style={{ transform: rotateLeftEye }}>
-          <div className={styles.pupil} />
-        </div>
-        {/* <div id="right-eye" className={styles.eye} style={{ transform: rotateRightEye }}>
-          <div className={styles.pupil} />
+    <>
+      <div className={styles.eyesFollow}>
+        <div className={styles.eyes}>
+          <div id="left-eye" className={styles.eye} style={{ transform: rotateLeftEye }}>
+            <div className={styles.pupil} />
+          </div>
+          {/* Remove the second eye for now
+        <div id="right-eye" className={styles.eye} style={{ transform: rotateRightEye }}>
+          <div className={styles.pupil}  />
         </div> */}
+        </div>
       </div>
-    </div>
+
+
+      <div>
+        <div>
+          <h1>Hello</h1>
+
+
+        </div>
+      </div>
+
+    </>
   );
 };
 
