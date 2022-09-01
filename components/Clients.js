ed57761efc898296758bdb5aa314ade70c7d3270
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-09-01 10:57:34 -0400

    modules

--- components/Clients.js
+++ components/Clients.js
@@ -7,132 +7,142 @@ import Image from "next/image"
 import Link from "next/link"
 
 
-const props = {
-    header: 'woo',
-    intro: 'Hello, I\'m Kristina Groeger 👋🏻.',
-    headertext: ' I\'m a UIX Developer & Designer with over 10 years of experience. \n I build front-end experiences and solutions with a focus on accessibility, interaction, and ease of use.',
-    buttontext: 'Have a Look'
-}
-
 export default function Clients() {
     {
 
         return (
             <>
 
-                <Container>
+
+                <div className={styles.relative}>
                     <div className={styles.relative}>
-                        <div className={styles.relative}>
-                            <div className={styles.clients}>
-                                <div className={styles.positionCANLOGO}>
-                                    <Link href="/" >
-                                        <div className={styles.canadaLogo}>
-                                            <Image className="canadaLogo"
-                                                width={120}
-                                                height={120}
-                                                src={images.canadaLogo}
-                                                alt=" Canada logo"
-                                            />
-                                        </div>
-                                    </Link>
-                                </div>
-
-                                <div className={styles.positionCAPLOGO}>
-                                    <Link href="/" >
-                                        <div className={styles.capitalOneLogo}>
-                                            <Image className="capitalOneLogo"
-                                                width={120}
-                                                height={120}
-                                                src={images.capitalOneLogo}
-                                                alt=" Capital One logo"
-                                            />
-                                        </div>
-                                    </Link>
-                                </div>
-
-                                <div className={styles.positionINTLOGO}>
-                                    <Link href="/" >
-                                        <div className={styles.intuitLogo}>
-                                            <Image className="intuitLogo"
-                                                width={120}
-                                                height={120}
-                                                src={images.intuitLogo}
-                                                alt=" Intuit logo"
-                                            />
-                                        </div>
-                                    </Link>
-                                </div>
-
-                                <div className={styles.positionKALOGO}>
-                                    <Link href="/" >
-                                        <div className={styles.kitchenaidLogo}>
-                                            <Image className="kitchenaidLogo"
-                                                width={120}
-                                                height={120}
-                                                src={images.kitchenaidLogo}
-                                                alt="Kitchenaid logo"
-                                            />
-                                        </div>
-                                    </Link>
-                                </div>
-
-                                <div className={styles.positionLOBLOGO}>
-                                    <Link href="/" >
-                                        <div className={styles.loblawsLogo}>
-                                            <Image className="loblawsLogo"
-                                                width={120}
-                                                height={120}
-                                                src={images.loblawsLogo}
-                                                alt="Loblaws logo"
-                                            />
-                                        </div>
-                                    </Link>
-                                </div>
-
-
-                                <div className={styles.positionMAYLOGO}>
-                                    <Link href="/" >
-                                        <div className={styles.maytagLogo}>
-                                            <Image className="maytagLogo"
-                                                width={120}
-                                                height={120}
-                                                src={images.maytagLogo}
-                                                alt="Maytag logo"
-                                            />
-                                        </div>
-                                    </Link>
-                                </div>
-
-
-                                <div className={styles.positionMCGLOGO}>
-                                    <Link href="/" >
-                                        <div className={styles.universityOfMcGillLogo}>
-                                            <Image className="universityOfMcGillLogo"
-                                                width={120}
-                                                height={120}
-                                                src={images.universityOfMcGillLogo}
-                                                alt="University of McGill logo"
-                                            />
-                                        </div>
-                                    </Link>
-                                </div>
-
-                                <div className={styles.positionWPLOGO}>
-                                    <Link href="/" >
-                                        <div className={styles.whirlpoolLogo}>
-                                            <Image className="whirlpoolLogo"
-                                                width={120}
-                                                height={120}
-                                                src={images.whirlpoolLogo}
-                                                alt="Whirlpool  logo"
-                                            />
-                                        </div>
-                                    </Link>
-                                </div>
+                        <div className={styles.clients}>
+                            <div className={styles.positionCANLOGO}>
+                                <Link href="/" >
+                                    <div className={styles.canadaLogo}>
+                                        <Image className="canadaLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.canadaLogo}
+                                            alt=" Canada logo"
+                                        />
+                                    </div>
+                                </Link>
                             </div>
+
+                            <div className={styles.positionCAPLOGO}>
+                                <Link href="/" >
+                                    <div className={styles.capitalOneLogo}>
+                                        <Image className="capitalOneLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.capitalOneLogo}
+                                            alt=" Capital One logo"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+
+                            <div className={styles.positionINTLOGO}>
+                                <Link href="/" >
+                                    <div className={styles.intuitLogo}>
+                                        <Image className="intuitLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.intuitLogo}
+                                            alt=" Intuit logo"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+
+                            <div className={styles.positionKALOGO}>
+                                <Link href="/" >
+                                    <div className={styles.kitchenaidLogo}>
+                                        <Image className="kitchenaidLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.kitchenaidLogo}
+                                            alt="Kitchenaid logo"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+
+                            <div className={styles.positionLOBLOGO}>
+                                <Link href="/" >
+                                    <div className={styles.loblawsLogo}>
+                                        <Image className="loblawsLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.loblawsLogo}
+                                            alt="Loblaws logo"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+
+
+                            <div className={styles.positionMAYLOGO}>
+                                <Link href="/" >
+                                    <div className={styles.maytagLogo}>
+                                        <Image className="maytagLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.maytagLogo}
+                                            alt="Maytag logo"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+
+
+                            <div className={styles.positionMCGLOGO}>
+                                <Link href="/" >
+                                    <div className={styles.universityOfMcGillLogo}>
+                                        <Image className="universityOfMcGillLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.universityOfMcGillLogo}
+                                            alt="University of McGill logo"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+
+                            <div className={styles.positionWPLOGO}>
+                                <Link href="/" >
+                                    <div className={styles.whirlpoolLogo}>
+                                        <Image className="whirlpoolLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.whirlpoolLogo}
+                                            alt="Whirlpool  logo"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+
+
+
+                            <div className={styles.positionCANLOGO}>
+                                <Link href="/" >
+                                    <div className={styles.canadaLogo}>
+                                        <Image className="canadaLogo"
+                                            width={120}
+                                            height={120}
+                                            src={images.canadaLogo}
+                                            alt=" Canada logo"
+                                        />
+                                    </div>
+                                </Link>
+                            </div>
+
+
                         </div>
                     </div>
-                </Container>
+                </div>
+
             </>
         );
     }
--- components/HeroWork.js
+++ components/HeroWork.js
@@ -11,31 +11,34 @@ export default function Hero({ }) {
 
     const props = {
         header: 'Work Experience',
-        intro: 'In my 10 years as a web developer and designer, I\'ve had the opportunity to work with a variety of large brands. Often, brands within brands.',
+        intro: 'In my decade as a web developer and designer, I\'ve had the opportunity to work with a variety of large brands. Often, brands within brands.',
         headertext: 'I\'m familiar with adhearing to strict brand guidelines, AODA standards, style guides, writing documentation, and mentoring. In addition to development and design, I love working with animation, interaction design, UX review, & illustration.',
         buttontext: 'Projects'
     }
 
     return (
-        <Container >
-            <div className={styles.hero}>
-                <Box className={styles.heroBox01}>
-                    <h1 className="font-serif">{props.header}</h1>
-                    <p className={styles.pmargin}>{props.intro}</p>
-                    <p>{props.headertext}</p>
-                    <Button className="buttons">{props.buttontext}&nbsp;<SouthIcon /></Button>
-                </Box>
-                <Box className={styles.heroBox02}>
-                    <div>
-                        <Image
-                            src={Images.growth}
-                            alt="A woman climbing the steps of her career" />
-                    </div>
-                </Box>
 
+        <div className={styles.hero}>
+            <Container>
+                <div className={styles.content}>
+                    <Box className={styles.heroBox01}>
+                        <h1 className="font-serif ">{props.header}</h1>
+                        <p className={styles.pmargin}>{props.intro}</p>
+                        <p>{props.headertext}</p>
+                        <Button className="buttons">{props.buttontext}&nbsp;<SouthIcon /></Button>
+                    </Box>
+                    <Box className={styles.heroBox02}>
+                        <div>
+                            <Image
+                                src={Images.growth}
+                                alt="A woman climbing the steps of her career" />
+                        </div>
+                    </Box>
+                </div>
+            </Container>
 
-            </div>
-        </Container>
+        </div>
+
 
 
     )
