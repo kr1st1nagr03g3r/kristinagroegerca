10eb982676f0a4524e9f5d16ef398dc12d02c1d7
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-09-05 11:43:35 -0400

    hero mobile

--- pages/temp.js
+++ pages/temp.js
@@ -1,28 +1,41 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+import React from 'react';
+import styles from '../styles/animatedAssets/AnimateNav.module.css'
+import { useInView } from 'react-intersection-observer';
+import Starfield from '../components/animatedAssets/Starfield';
+import Hero from '../components/Hero';
+import Navigation from "../components/Navigation"
 
-export default function FullWidthGrid() {
+
+
+export default function Component() {
+
+    // const { ref, inView, entry } = useInView({
+    //     /* Optional options */
+    //     threshold: 0.5,
+    // });
+
+
+    // this is how to name the variables so you may use multiple on a page.
+    const {
+        ref: magicSectionRef,
+        inView: magicSectionIsVisible } = useInView({
+            threshold: 0.3,
+        });
+
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                    <Item><h2>xs=12 md=6</h2></Item>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Item><h2>xs=12 md=6</h2></Item>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+        <>
+            <span className={`${styles.rocket} ${magicSectionIsVisible ? styles.animateRocket : ''}`}>
+                🚀
+                <Navigation />
+            </span>
+
+            <Hero />
+            <span ref={magicSectionRef}>
+                <Starfield /></span>
+
+
+        </>
+    )
 }
