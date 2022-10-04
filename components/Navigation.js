5816e58bd8d5db8aa7bdd1cf75777a11806974e4
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-10-04 15:24:17 -0400

    🥤

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,30 +1,41 @@
-
-import { Container } from "@mui/system"
-import Link from "next/link"
-import { useInView } from 'react-intersection-observer';
-import styles from '../styles/animatedAssets/AnimateNav.module.css'
+import { Container } from '@mui/system'
+import Link from 'next/link'
+import styles from '../styles/animatedAssets/Animations.module.css'
 
 export default function Nav() {
+  return (
+    <>
+      <Container
+        className={styles.nav}
+        maxWidth={false}
+        disableGutters={true}
+        sx={{ display: 'flex' }}
+      >
+        <Container className={styles.navInner}>
+          <div className="logo">
+            <Link href="/">
+              <a>
+                <i>Kristina Groeger</i>
+              </a>
+            </Link>
+          </div>
 
-
-
-    return (
-        <>
-            <Container className={styles.nav} maxWidth={false} disableGutters={true}>
-                <Container className={styles.navInner}>
-                    <div className="logo">
-                        <Link href="/"><a><i>Kristina Groeger</i></a></Link>
-                    </div>
-
-                    <div className="links">
-                        <Link href="/"><a className={styles.home}>Home</a></Link>
-                        <Link href="/work" ><a className={styles.work}>Work</a></Link>
-                        <Link href="/about"><a className={styles.about}>About</a></Link>
-                        <Link href="/contact"><a className={styles.contact}>Contact</a></Link>
-                    </div>
-                </Container>
-            </Container>
-        </>
-    )
+          <div className="links">
+            <Link href="/">
+              <a className={styles.home}>Home</a>
+            </Link>
+            <Link href="/work">
+              <a className={styles.work}>Work</a>
+            </Link>
+            <Link href="/about">
+              <a className={styles.about}>About</a>
+            </Link>
+            <Link href="/contact">
+              <a className={styles.contact}>Contact</a>
+            </Link>
+          </div>
+        </Container>
+      </Container>
+    </>
+  )
 }
-
