fd85983216cfc0a4bfbc78299f5addaa344cf6d8
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-10-06 18:05:17 -0400

    starfield mobile and fixing animation

--- components/animatedAssets/Starfield.js
+++ components/animatedAssets/Starfield.js
@@ -1,22 +1,17 @@
 import styles from '/styles/animatedAssets/Starfield.module.css'
-import Image from 'next/image'
-import Images from '../../public/home/index.js'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
-import Link from '@mui/material/Link'
+import Logos from './Logos.js'
 
 export default function Starfield({}) {
   const props = {
-    //     starfieldHead01: "I'm comfortable  ",
-    //     starfieldHead02: 'working with a diverse ',
-    //     starfieldHead03: 'range of tech & tools',
     buttontext: 'see more',
   }
 
   return (
     <>
-      <Container className={` scroller ${styles.container}`}>
+      <Container maxWidth={true} className={styles.container}>
         <section>
           <span className={styles.star}>.</span>
           <span className={styles.star}>.</span>
@@ -44,252 +39,38 @@ export default function Starfield({}) {
           <span className={styles.star}>.</span>
           <span className={styles.star}>.</span>
         </section>
-        <Container className={styles.whatever}>
-          <Box sx={{ width: '50%' }}>
-            <div className={` spaceStuff ${styles.spaceStuff}`}>
-              <div className={styles.align}>
-                <div className={styles.positionJS}>
-                  <Link href="/work">
-                    <div className={styles.js}>
-                      <Image
-                        className="js"
-                        width={60}
-                        height={60}
-                        src={Images.js}
-                        alt="White Javascript logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionReact}>
-                  <Link href="/work">
-                    <div className={styles.react}>
-                      <Image
-                        className="react"
-                        width={60}
-                        height={60}
-                        src={Images.react}
-                        alt="White React logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionNextjs}>
-                  <Link href="/work">
-                    <div className={styles.nextjs}>
-                      <Image
-                        className="nextjs"
-                        width={60}
-                        height={60}
-                        src={Images.nextjs}
-                        alt="White nextjs logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionVercel}>
-                  <Link href="/work">
-                    <div className={styles.vercel}>
-                      <Image
-                        className="vercel"
-                        width={80}
-                        height={80}
-                        src={Images.vercel}
-                        alt="White vercel logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-              </div>
-
-              <div className={styles.align}>
-                <div className={styles.positionGH}>
-                  <Link href="/work">
-                    <div className={styles.gh}>
-                      <Image
-                        className="gh"
-                        width={60}
-                        height={60}
-                        src={Images.github}
-                        alt="White Github logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionCSS}>
-                  <Link href="/work">
-                    <div className={styles.css}>
-                      <Image
-                        className="html"
-                        width={60}
-                        height={60}
-                        src={Images.css}
-                        alt="White CSS logo"
-                      />
-                      <p className={styles.html5}>CSS</p>
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionaem}>
-                  <Link href="/work">
-                    <div className={styles.aem}>
-                      <Image
-                        className="aem"
-                        width={60}
-                        height={60}
-                        src={Images.aem}
-                        alt="White aem logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionNode}>
-                  <Link href="/work">
-                    <div className={styles.nodejs}>
-                      <Image
-                        className="nodejs"
-                        width={60}
-                        height={60}
-                        src={Images.nodejs}
-                        alt="White node logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-              </div>
-
-              <div className={styles.align}>
-                <div className={styles.positionHTML}>
-                  <Link href="/work">
-                    <div className={styles.html}>
-                      <Image
-                        className="html"
-                        width={60}
-                        height={60}
-                        src={Images.html}
-                        alt="White HTML logo"
-                      />
-                      <p className={styles.html5}>HTML</p>
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionSASS}>
-                  <Link href="/work">
-                    <div className={styles.sass}>
-                      <Image
-                        className="sass"
-                        width={60}
-                        height={60}
-                        src={Images.sass}
-                        alt="White Sass logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionTailwind}>
-                  <Link href="/work">
-                    <div className={styles.tailwind}>
-                      <Image
-                        className="tailwind"
-                        width={60}
-                        height={60}
-                        src={Images.tailwind}
-                        alt="White Sass logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionMUI}>
-                  <Link href="/work">
-                    <div className={styles.mui}>
-                      <Image
-                        className="mui"
-                        width={60}
-                        height={60}
-                        src={Images.mui}
-                        alt="White MUI logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-              </div>
-
-              <div className={styles.align}>
-                <div className={styles.positionFigma}>
-                  <Link href="/work">
-                    <div className={styles.figma}>
-                      <Image
-                        className="figma"
-                        width={60}
-                        height={60}
-                        src={Images.figma}
-                        alt="White photoshop logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionAE}>
-                  <Link href="/work">
-                    <div className={styles.AE}>
-                      <Image
-                        className="AE"
-                        width={60}
-                        height={60}
-                        src={Images.AE}
-                        alt="White Sass logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionIllustrator}>
-                  <Link href="/work">
-                    <div className={styles.illustrator}>
-                      <Image
-                        className="illustrator"
-                        width={60}
-                        height={60}
-                        src={Images.illustrator}
-                        alt="White Sass logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-
-                <div className={styles.positionPhotoshop}>
-                  <Link href="/work">
-                    <div className={styles.photoshop}>
-                      <Image
-                        className="photoshop"
-                        width={60}
-                        height={60}
-                        src={Images.photoshop}
-                        alt="White photoshop logo"
-                      />
-                    </div>
-                  </Link>
-                </div>
-              </div>
-            </div>
+        <Container
+          className={styles.wrapper}
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignContent: 'center',
+            justifyContent: 'center',
+            alignItems: 'left',
+            width: '80%',
+            position: 'relative',
+          }}
+        >
+          <Box sx={{ width: '50%' }} className={styles.boxWrapper}>
+            <Logos />
           </Box>
-          <Box className={styles.boxOne} sx={{ width: '50%' }}>
+          <Box
+            sx={{
+              width: '50%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'flex-end',
+            }}
+            className={styles.boxWrapper}
+          >
             <div className={styles.content}>
-              <h2 className="font-serif white">
+              <h2 className="font-serif white right">
                 I&apos;m comfortable working with a diverse range of tech and
                 tools
               </h2>
               <div className={styles.button}>
-                <Button className={` buttons ${styles.buttons}`}>
+                <Button className={` buttons ${styles.buttons}`} href="/work">
                   {props.buttontext}
                 </Button>
               </div>
