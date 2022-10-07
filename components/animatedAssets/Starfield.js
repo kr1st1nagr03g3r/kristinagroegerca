2e031ff25c60aec5baebc908514bf407cd744c16
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-10-07 12:07:59 -0400

    starfield update "md" MUI

--- components/animatedAssets/Starfield.js
+++ components/animatedAssets/Starfield.js
@@ -16,70 +16,72 @@ export default function Starfield({}) {
       margin: 0px;
       }`}</style>
       <Container maxWidth={true} className={styles.container}>
-        <section>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-          <span className={styles.star}>.</span>
-        </section>
-        <Container
-          className={styles.wrapper}
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignContent: 'center',
-            justifyContent: 'center',
-            alignItems: 'left',
-            width: '80%',
-            position: 'relative',
-          }}
-        >
-          <Box sx={{ width: '50%' }} className={styles.boxWrapper02}>
-            <Logos />
-          </Box>
-          <Box
+        <Container maxWidth="lg">
+          <section>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+            <span className={styles.star}>.</span>
+          </section>
+          <Container
+            className={styles.wrapper}
             sx={{
-              width: '50%',
               display: 'flex',
-              flexDirection: 'column',
+              flexDirection: 'row',
+              alignContent: 'center',
               justifyContent: 'center',
-              alignItems: 'flex-end',
+              alignItems: 'left',
+              width: '80%',
+              position: 'relative',
             }}
-            className={styles.boxWrapper01}
           >
-            <div className={styles.content}>
-              <h2 className="font-serif white right">
-                I&apos;m comfortable working with a diverse range of tech and
-                tools
-              </h2>
-              <div className={styles.button}>
-                <Button className={` buttons ${styles.buttons}`} href="/work">
-                  <p className="white">{props.buttontext}</p>
-                </Button>
+            <Box sx={{ width: '50%' }} className={styles.boxWrapper02}>
+              <Logos />
+            </Box>
+            <Box
+              sx={{
+                width: '50%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'flex-end',
+              }}
+              className={styles.boxWrapper01}
+            >
+              <div className={styles.content}>
+                <h2 className="font-serif white right">
+                  I&apos;m comfortable working with a diverse range of tech and
+                  tools
+                </h2>
+                <div className={styles.button}>
+                  <Button className={` buttons ${styles.buttons}`} href="/work">
+                    <p className="white">{props.buttontext}</p>
+                  </Button>
+                </div>
               </div>
-            </div>
-          </Box>
+            </Box>
+          </Container>
         </Container>
       </Container>
     </>
