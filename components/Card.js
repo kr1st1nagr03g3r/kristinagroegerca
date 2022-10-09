59b879a8dfdec063177e4d9a54a0106d3b152f53
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-10-09 11:51:06 -0400

    removed breaking '../styles/CardContent.module.css

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,9 @@
-import styles from '../styles/CardContent.module.css'
+// import styles from '../styles/CardContent.module.css'
 
 export default function CardContent({ children }) {
   return (
     <>
-      <div className={styles.card}>{children}</div>
+      <div>{children}</div>
     </>
   )
 }
