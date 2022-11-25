85155af0b8883df8028f6a790fe1327ae8ac2e90
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-11-25 14:36:38 -0500

    sendgrid api

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,7 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const mail = require('@sendgrid/mail')
+
+mail.setApiKey(sendgrid.env.SENDGRID_API_KEY)
+
 export default function handler(req, res) {
   const body = JSON.parse(req.body)
   console.log(body)
+
+  const message = `
+  Name:  ${body.name}\r\n
+  Email:  ${body.email}\r\n
+  Message:  ${body.message}\r\n
+  `
+
+  const data = {
+    to: 'me@kristinagroeger.ca',
+    from: 'me@kristinagroeger.ca',
+    subject: 'new web form message',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  }
+
+  mail.send(data)
+
   res.status(200).json({ name: 'Ok' })
 }
